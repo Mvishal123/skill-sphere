f4225f03a1d515e5587d9f56e6bc5a4eefd4f91b
Vishal-m <vishalmahesh.study@gmail.com>
2024-02-07 01:54:40 +0530

    ui: course card

--- app/(user)/courses/page.tsx
+++ app/(user)/courses/page.tsx
@@ -1,3 +1,4 @@
+import CourseCard from "@/components/course/course-card";
 import Sidebar from "@/components/course/sidebar";
 import { db } from "@/db";
 import { Ghost } from "lucide-react";
@@ -45,11 +46,33 @@ const CoursePage = async ({ searchParams }: SearchParamsProps) => {
         <div className="p-10">
           <h1 className="text-3xl font-bold">Courses</h1>
         </div>
-        <div>
-          {courses && (
+        <div className="px-10">
+          {!courses && (
             <div className="h-[200px] w-full flex flex-col justify-end items-center text-xl gap-2">
               <Ghost className="h-10 w-10" />
               <p> No course </p>
+            </div>
+          )}
+          {courses && (
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+              {courses.map((course) => {
+                let rating = 0;
+                course.CourseReview.forEach((review) => {
+                  rating += review.rating;
+                });
+                return (
+                  <div key={course.id} className="w-200px h-150px">
+                    <CourseCard
+                      id={course.id}
+                      title={course.title}
+                      category={course.category!}
+                      price={course.cost!}
+                      imageUrl={course.image!}
+                      rating={4.5}
+                    />
+                  </div>  
+                );
+              })}
             </div>
           )}
         </div>
--- components/course/course-card.tsx
+++ components/course/course-card.tsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import React from "react";
+import { Card, CardContent } from "../ui/card";
+import { Rating } from "@mui/material";
+import Image from "next/image";
+import { currencyConverter } from "@/lib/currency-converter";
 
-const CourseCard = () => {
-  return (
-    <div>CourseCard</div>
-  )
+interface CourseCardProps {
+  id: string;
+  title: string;
+  imageUrl: string;
+  category: string;
+  price: number;
+  rating: number;
 }
+const CourseCard = ({
+  id,
+  category,
+  imageUrl,
+  price,
+  rating,
+  title,
+}: CourseCardProps) => {
+  return (
+    <Card className="p-1 w-[300px] h-[260px]">
+      <div className="aspect-video relative overflow-hidden border rounded-lg">
+        <Image
+          src={imageUrl}
+          alt={title}
+          fill
+          className="rounded-md object-center"
+        />
+      </div>
+      <div>
+        <h1 className="text-lg font-bold mt-1 line-clamp-2">{title}</h1>
+        <span className="text-xs font-semibold text-slate-500">{category}</span>
+      </div>
+      <div className="flex justify-between mt-2">
+        <div className="font-semibold">{currencyConverter(price)}</div>
+        <div>
+          <Rating size="small" value={rating} readOnly />
+        </div>
+      </div>
+    </Card>
+  );
+};
 
-export default CourseCard
+export default CourseCard;
